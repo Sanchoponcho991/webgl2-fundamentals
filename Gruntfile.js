9326d68524fcc2da19ec1f3604f1750c94196a14
Sanchoponcho991 <131296058+Sanchoponcho991@users.noreply.github.com>
2023-07-03 23:11:37 +0300

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,200 @@
+/*eslint-env node*/
+
+'use strict';
+
+process.on('unhandledRejection', up => {
+  throw up;
+});
+
+const fs = require('fs');
+const path = require('path');
+const liveEditor = require('@gfxfundamentals/live-editor');
+const liveEditorPath = path.dirname(require.resolve('@gfxfundamentals/live-editor'));
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  const s_ignoreRE = /\.(md|py|sh|enc)$/i;
+  function noMds(filename) {
+    return !s_ignoreRE.test(filename);
+  }
+
+  const s_isMdRE = /\.md$/i;
+  function mdsOnly(filename) {
+    return s_isMdRE.test(filename);
+  }
+
+  function notFolder(filename) {
+    return !fs.statSync(filename).isDirectory();
+  }
+
+  function noMdsNoFolders(filename) {
+    return noMds(filename) && notFolder(filename);
+  }
+
+  grunt.initConfig({
+    eslint: {
+      lib: {
+        src: [
+          'webgl/resources/webgl-utils.js',
+          'webgl/resources/lessons-helper.js',
+          'webgl/resources/flattened-primitives.js',
+          'webgl/resources/2d-math.js',
+          'webgl/resources/3d-math.js',
+          'build/js/*.js',
+        ],
+      },
+      examples: {
+        src: [
+          'webgl/*.html',
+          '!webgl/webgl-qna-*.html',
+          // 'webgl/lessons/*.md',
+        ],
+      },
+      diagram: {
+        src: [
+          'webgl/lessons/resources/webgl-state-diagram/*.js',
+        ],
+      },
+    },
+    jsdoc: {
+      docs: {
+        src: [
+          'webgl/resources/2d-math.js',
+          'webgl/resources/3d-math.js',
+          'webgl/resources/webgl-utils.js',
+          'docs.md',
+        ],
+        options: {
+          destination: 'out/docs',
+          configure: 'build/conf/jsdoc.conf.json',
+          template: './node_modules/minami',
+        },
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          { expand: false, src: '*', dest: 'out/', filter: noMdsNoFolders, },
+          { expand: true, cwd: `${liveEditor.monacoEditor}/`, src: 'min/**', dest: 'out/monaco-editor/', nonull: true, },
+          { expand: true, cwd: `${liveEditorPath}/src/`, src: '**', dest: 'out/webgl/resources/', nonull: true, },
+          { expand: true, src: 'webgl/**', dest: 'out/', filter: noMds, },
+          { expand: true, src: '3rdparty/**', dest: 'out/', },
+        ],
+      },
+    },
+    clean: [
+      'out/**/*',
+    ],
+    buildlesson: {
+      main: {
+        files: [],
+      },
+    },
+    watch: {
+      main: {
+        files: [
+          'webgl/**',
+          '3rdparty/**',
+          'node_modules/@gfxfundamentals/live-editor/src/**',
+        ],
+        tasks: ['copy'],
+        options: {
+          spawn: false,
+        },
+      },
+      lessons: {
+        files: [
+          'webgl/lessons/**/webgl*.md',
+        ],
+        tasks: ['buildlesson'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
+
+  let changedFiles = {};
+  const onChange = grunt.util._.debounce(function() {
+    grunt.config('copy.main.files', Object.keys(changedFiles).filter(noMds).map((file) => {
+      const copy = {
+        src: file,
+        dest: 'out/',
+      };
+      if (file.indexOf('live-editor') >= 0) {
+        copy.cwd = `${path.dirname(file)}/`;
+        copy.src = path.basename(file);
+        copy.expand = true;
+        copy.dest = 'out/webgl/resources/';
+      }
+      return copy;
+    }));
+    grunt.config('buildlesson.main.files', Object.keys(changedFiles).filter(mdsOnly).map((file) => {
+      return {
+        src: file,
+      };
+    }));
+    changedFiles = {};
+  }, 200);
+  grunt.event.on('watch', function(action, filepath) {
+    changedFiles[filepath] = action;
+    onChange();
+  });
+
+  const buildSettings = {
+    outDir: 'out',
+    baseUrl: 'https://webgl2fundamentals.org',
+    rootFolder: 'webgl',
+    lessonGrep: 'webgl*.md',
+    siteName: 'WebGL2Fundamentals',
+    siteThumbnail: 'webgl2fundamentals.jpg',  // in rootFolder/lessons/resources
+    templatePath: 'build/templates',
+    owner: 'gfxfundamentals',
+    repo: 'webgl2-fundamentals',
+    thumbnailOptions: {
+      thumbnailBackground: 'webgl2fundamentals.jpg',
+      text: [
+        {
+          font: 'bold 100px lesson-font',
+          verticalSpacing: 100,
+          offset: [100, 120],
+          textAlign: 'left',
+          shadowOffset: [15, 15],
+          strokeWidth: 15,
+          textWrapWidth: 1000,
+        },
+      ],
+    },
+  };
+
+  // just the hackiest way to get this working.
+  grunt.registerMultiTask('buildlesson', 'build a lesson', function() {
+    const filenames = new Set();
+    this.files.forEach((files) => {
+      files.src.forEach((filename) => {
+        filenames.add(filename);
+      });
+    });
+    const buildStuff = require('@gfxfundamentals/lesson-builder');
+    const settings = Object.assign({}, buildSettings, {
+      filenames,
+    });
+    const finish = this.async();
+    buildStuff(settings).finally(finish);
+  });
+
+  grunt.registerTask('buildlessons', function() {
+    const buildStuff = require('@gfxfundamentals/lesson-builder');
+    const finish = this.async();
+    buildStuff(buildSettings).finally(finish);
+  });
+
+  grunt.registerTask('build', ['clean', 'copy', 'buildlessons']);
+  grunt.registerTask('buildwatch', ['build', 'watch']);
+  grunt.registerTask('pre-push', ['eslint']);
+
+  grunt.registerTask('default', ['eslint', 'build', 'jsdoc']);
+};
+
---
+++ docs.md
@@ -0,0 +1,28 @@
+WebGL2Fundamentals API Docs
+==========================
+
+These docs are an attempt to help explain some of the helper function
+used in the articles on [webgl2fundamentals.org](https://webgl2fundamentals.org).
+
+The main ones are in [webgl-utils.js](/webgl/resources/webgl-utils.js) and basically include
+just 2 commonly used functions. One is `webglUtils.createProgramsFromScripts`. How it works
+[is described here](/webgl/lessons/webgl-boilerplate.html). The other common function is
+`webglUtils.resizeCanvasToDisplaySize` which [is described in this lesson](/webgl/lessons/webgl-resizing-the-canvas.html).
+
+There's also another library in a few examples called TWGL. You can find [that library and its documentation
+here](https://twgljs.org).
+
+If you're using WebGL to do 2D or 3D you'll likely need many math helper functions
+in JavaScript as well.
+
+[Functions for 2D math are here](/docs/module-webgl-2d-math.html).
+
+[Functions for 3D math are here](/docs/module-webgl-3d-math.html).
+
+# JSDoc3 Plea
+
+These docs are generated with [JSDoc3](https://usejsdoc.org/). I find them very confusing to look at
+and in some places frustratingly verbose. If you have any experience with JSDoc3 and know how to
+make the docs more approachable and useful please [submit a pull request](https://github.com/gfxfundamentals/webgl2-fundamentals)
+or [file an issue with details](https://github.com/gfxfundamentals/webgl2-fundamentals/issues).
+
